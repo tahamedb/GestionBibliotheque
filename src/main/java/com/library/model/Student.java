7d6ccf64a3ab64f2c929da6eb5fe9a46a93c1e90
taha <tahabouataoui5@gmail.com>
2024-12-20 12:42:56 +0100

    first commit

---
+++ src/main/java/com/library/model/Student.java
@@ -0,0 +1,36 @@
+package com.library.model;
+
+public class Student {
+    private int id;
+    private String name;
+
+    // Constructeur par défaut
+    public Student() {
+    }
+    public Student(String name){
+        this.name = name;
+    }
+
+    // Constructeur complet
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Getters et Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
