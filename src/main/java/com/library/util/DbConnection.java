5ed43fb6d04d59badd763361fd56e54cf295f174
taha <tahabouataoui5@gmail.com>
2024-12-20 16:30:51 +0100

    test commit

--- src/main/java/com/library/util/DbConnection.java
+++ src/main/java/com/library/util/DbConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/dbLibrary";
+    private static final String URL = "jdbc:mysql://localhost:3306/Library";
     private static final String USER = "root";
     private static final String PASSWORD = "password";
 
